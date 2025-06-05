d88edec245fb5b84f6281b923e2e83f6b9b9b5e4
tubo <tubo@tubo.com>
2025-06-06 01:11:34 +0800

    mark前归档

--- projects/web_api/entrypoint.sh
+++ projects/web_api/entrypoint.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 # 激活虚拟环境
-. /app/venv/bin/activate
+. /app/.venv/bin/activate
 
 # 设置配置文件路径
 export CONFIG_PATH="/app/config.yaml"
@@ -11,4 +11,4 @@ export CONFIG_PATH="/app/config.yaml"
 mkdir -p /app/output
 
 # 启动应用
-exec venv/bin/uvicorn app:app "$@"
+exec .venv/bin/uvicorn app:app "$@"
