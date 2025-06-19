ebf5432931498926d3c5d5f12e7afd48578595f2
Myth <myth@tubo.com>
2025-06-19 11:32:48 +0800

    添加模型

--- projects/web_api/download_models-old.py
+++ projects/web_api/download_models-old.py
@@ -41,8 +41,8 @@ if __name__ == '__main__':
         # "models/TabRec/TableMaster/*",
         # "models/TabRec/StructEqTable/*",
     ]
-    model_dir = snapshot_download('opendatalab/PDF-Extract-Kit-1.0', allow_patterns=mineru_patterns, local_dir="models/miner-u")
-    layoutreader_model_dir = snapshot_download('ppaanngggg/layoutreader', local_dir="models/layout-reader")
+    model_dir = snapshot_download('opendatalab/PDF-Extract-Kit-1.0', allow_patterns=mineru_patterns, local_dir="models/miner-u/miner-u")
+    layoutreader_model_dir = snapshot_download('ppaanngggg/layoutreader', local_dir="models/miner-u/layout-reader")
     model_dir = model_dir + '/models'
     print(f'model_dir is: {model_dir}')
     print(f'layoutreader_model_dir is: {layoutreader_model_dir}')
